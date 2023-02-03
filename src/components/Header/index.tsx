2448b3d6e3fbe3a0fc8142f498391a5d867b3031
douglasor <douglasor@outlook.com>
2023-02-03 19:34:02 -0300

    feat(Components/NewTransactionModal):Create a modal UI and put it in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
+
 import logoImage from "./assets/logo.svg";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 
 export function Header() {
     return (
         <HeaderContainer>
+
             <HeaderContent>
                 <img src={logoImage} alt="" />
-                <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+                <Dialog.Root>
+
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova Transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+
+                    <NewTransactionModal />
+
+                </Dialog.Root>
+
             </HeaderContent>
+
         </HeaderContainer>
     )
 }
