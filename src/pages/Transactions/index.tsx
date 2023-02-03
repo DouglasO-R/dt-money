5586a5c0ae42b0d51107e9eadc50663102f44ce4
douglasor <douglasor@outlook.com>
2023-02-03 18:31:09 -0300

    feat(Pages/Transactions):Create components UI for search form

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionList } from "../../components/TransactionList";
+import { SearchForm } from "./components/SearchForm";
 
 
 export function Transactions() {
@@ -8,6 +9,7 @@ export function Transactions() {
         <>
             <Header />
             <Summary />
+            <SearchForm />
             <TransactionList />
         </>
     )
