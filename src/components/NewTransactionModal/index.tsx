f1fff0d76baaf22360fbc10d17d62891817a5547
douglasor <douglasor@outlook.com>
2023-02-04 18:19:08 -0300

    feat (Components/NewTransactionModal):Use react hook form to manipulate radio group with hook form Controller

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import * as zod from 'zod';
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./style";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 
@@ -12,7 +12,7 @@ const newTransactionFormSchema = zod.object({
     description: zod.string(),
     price: zod.number(),
     category: zod.string(),
-    // type: zod.enum(['income', 'outcome']),
+    type: zod.enum(['income', 'outcome']),
 });
 
 type NewTransactionFormInputs = zod.infer<typeof newTransactionFormSchema>;
@@ -21,13 +21,14 @@ export function NewTransactionModal() {
     const {
         register,
         handleSubmit,
+        control,
         formState: { isSubmitting }
     } = useForm<NewTransactionFormInputs>({
         resolver: zodResolver(newTransactionFormSchema)
     });
 
-    const handleCreateNewTransaction = () => {
-
+    const handleCreateNewTransaction = (data: NewTransactionFormInputs) => {
+        console.log(data)
     }
 
 
@@ -65,17 +66,26 @@ export function NewTransactionModal() {
                         {...register("category")}
                     />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant="income" value="income">
-                            <ArrowCircleUp size={24} />
-                            Entrada
-                        </TransactionTypeButton>
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({ field }) => (
+                            <TransactionType
+                                onValueChange={field.onChange}
+                                value={field.value}
+                            >
+                                <TransactionTypeButton variant="income" value="income">
+                                    <ArrowCircleUp size={24} />
+                                    Entrada
+                                </TransactionTypeButton>
 
-                        <TransactionTypeButton variant="outcome" value="outcome">
-                            <ArrowCircleDown size={24} />
-                            Saida
-                        </TransactionTypeButton>
-                    </TransactionType>
+                                <TransactionTypeButton variant="outcome" value="outcome">
+                                    <ArrowCircleDown size={24} />
+                                    Saida
+                                </TransactionTypeButton>
+                            </TransactionType>
+                        )}
+                    />
 
                     <button type="submit" disabled={isSubmitting}>
                         Cadastrar
